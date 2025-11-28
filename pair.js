da4c8101585df04532e34e113fc338b144d75acd
TKTTECH1 <tafadzwakureya799@gmail.com>
2025-11-28 13:55:12 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,17 +81,17 @@ router.get('/', async (req, res) => {
 
                         // Send video thumbnail with caption
                         await KnightBot.sendMessage(userJid, {
-                            image: { url: 'https://img.youtube.com/vi/-oz_u1iMgf8/maxresdefault.jpg' },
-                            caption: `🎬 *KnightBot MD V2.0 Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: https://youtu.be/NjOipI2AoMk`
+                            image: { url: 'https://files.catbox.moe/lfne89.png' },
+                            caption: `🎬 *TKT-CYBER_TECH BOT Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: https://youtu.be/tafadzwatkt`
                         });
                         console.log("🎬 Video guide sent successfully");
 
                         // Send warning message
                         await KnightBot.sendMessage(userJid, {
                             text: `⚠️Do not share this file with anybody⚠️\n 
-┌┤✑  Thanks for using Knight Bot
+┌┤✑  Thanks for using TKT-CYBER_TECH BOT
 │└────────────┈ ⳹        
-│©2025 Mr Unique Hacker 
+│©2025 TKT-TECH🇿🇼
 └─────────────────┈ ⳹\n\n`
                         });
                         console.log("⚠️ Warning message sent successfully");
